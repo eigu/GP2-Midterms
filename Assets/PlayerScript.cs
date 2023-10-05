d0bf650586c077762ec2ce3183eddfb2522f354d
eigu <jmpheen@gmail.com>
2023-10-06 00:59:30 +0800

    random spawn left

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
@@ -7,6 +8,8 @@ public class PlayerScript : MonoBehaviour
     public List<GameObject> playerColor;
     public GameObject player;
     public int playerColorCount = 0;
+    public Transform target;
+    public float range = 30f;
 
     void Awake()
     {
@@ -18,6 +21,12 @@ public class PlayerScript : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             ChangePlayerColor();
+        }
+
+        Vector3 relativePos = target.position - transform.position; 
+        if (relativePos.magnitude < range)
+        {
+            transform.rotation = Quaternion.LookRotation(relativePos, Vector3.up);
         }
     }
 
@@ -31,5 +40,22 @@ public class PlayerScript : MonoBehaviour
         }
 
         player.SetActive(true);
+    }
+
+    private void OnDrawGizmos()
+    {
+        float radius = range;
+
+        float angleStep = 360f / 360;
+
+        Gizmos.color = Color.white;
+        for (int i = 0; i < 360; i++)
+        {
+            float angle = i * angleStep;
+
+            Vector3 position = transform.position + radius * Vector3.right * Mathf.Cos(angle) + radius * Vector3.forward * Mathf.Sin(angle);
+
+            Gizmos.DrawLine(position, position + Vector3.right * 0.1f);
+        }
     }
 }
